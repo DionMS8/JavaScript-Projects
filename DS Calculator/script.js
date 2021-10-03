b660e934ec7171a2a19b8c70173aa9ea47b1cfb4
Dion Singh <dionsingh8@hotmail.com>
2021-10-03 10:38:56 -0400

    Started Creating a Calculator and added Styling

---
+++ DS Calculator/script.js
@@ -0,0 +1,16 @@
+let numbers = [];
+let operations = [];
+let currentNumber = 0;
+let operationMode = "false";
+
+// CREATING A FUNCTION TO BE EXECUTED WHEN A NUMBER 
+// IS PRESSED ON THE CALCULATOR AND
+
+function numPress(num) {
+
+}
+
+function operatorPress(op) {
+
+}
+
